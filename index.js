73c33325bb9a80f7ddcd453712c23c520826b8dc
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-19 12:35:53 +0600

    basic routing

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
 const app = express();
 
+//Basic Routing
 app.get("/", (req, res) => {
-  res.send("welcome to express js");
+  res.send("<h1>Welcome Home</h1>");
 });
-
-app.listen(8000, () => console.log("Server up"));
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+app.get("/contact", (req, res) => {
+  res.send("Contact us!");
+});
+//listening server
+app.listen(8008, () => console.log("Server up"));
