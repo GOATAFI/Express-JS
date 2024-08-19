df01dd292b30dab67ec05686ff3c0a4f00eef1b9
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-19 12:58:01 +0600

    http methods intro

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 const app = express();
 
+// ---------- HTTP METHODS ----------
+//GET -> Retrieve Data
+//POST -> Create / Insert data
+//PUT -> Completely Update Data
+//PATCH -> Partially Update Data
+//Delete -> Delete data
+
 //Basic Routing
 app.get("/", (req, res) => {
   res.send("<h1>Welcome Home</h1>");
