fb822882cee2be1f5159163e10f2b42bf363f541
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-21 21:54:41 +0600

    get put post delete student

--- index.js
+++ index.js
@@ -36,5 +36,19 @@ const cb3 = (req, res) => {
   res.send("Welcome to array callbacks");
 };
 app.get("/array-cb", [cb1, cb2, cb3]);
+
+app.get("/student", (req, res) => {
+  res.send("All Students");
+});
+app.post("/student", (req, res) => {
+  res.send("Add New Students");
+});
+app.put("/student", (req, res) => {
+  res.send("Update Students");
+});
+app.get("/student", (req, res) => {
+  res.send("Delete Students");
+});
+
 //listening server
 app.listen(8008, () => console.log("Server up"));
