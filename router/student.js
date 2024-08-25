6bb119556b3b6e19400992e0cd218050588ac10c
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-25 23:48:37 +0600

    student from router

--- router/student.js
+++ router/student.js
@@ -1,4 +1,9 @@
 import express from "express";
 const router = express.Router();
 
+router.get("/all", (req, res) => res.send("All Students"));
+router.get("/create", (req, res) => res.send("Create Students"));
+router.get("/update", (req, res) => res.send("Update Students"));
+router.get("/delete", (req, res) => res.send("Delete Students"));
+
 export default router;
